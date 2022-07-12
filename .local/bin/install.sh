c016e351f25b5d890b44385722fc0d479d10df77
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2022-07-12 00:57:58 -0400

    Add safety checks to install script

--- .local/bin/install.sh
+++ .local/bin/install.sh
@@ -3,10 +3,40 @@
 # Install config in your $HOME by running:
 # curl -FsSL https://raw.github.com/i13e/cfg/master/.local/bin/install.sh | /bin/sh
 
-sudo pacman -S --needed --asdeps --noconfirm base-devel git
+## Confirmation Dialog.
+while true; do
+    printf '%b ' "\033[1m":: Do not run this script unless you know EXACTLY what you are doing. It is recommended to be run only on a fresh install of Arch Linux as a non-root, wheel user. Do you wish to continue?  [Y/n]"\033[0m"
+    read -r yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer y or n.";;
+    esac
+done
 
-mkdir -p "$HOME/.config"
+## Safety Checks.
 
+# check if user is root
+
+# check if user is running Arch
+if command -v pacman >/dev/null 2>&1; then
+    # Install dependencies
+    sudo pacman -S --needed --noconfirm base-devel git
+    # Install paru
+    git clone https://aur.archlinux.org/paru-bin.git "/tmp/paru-bin"
+    cd "/tmp/paru-bin" || exit # exit in case cd fails
+    makepkg -si --noconfirm
+    cd "$HOME" && rm -rf "/tmp/paru-bin"
+else
+    echo "This script is only compatible with Arch Linux." ; exit 1
+fi
+
+## Install Dotfiles.
+
+# If config directory doesn't exist, make it
+[ ! -d "$HOME/.config" ] && mkdir -p "$HOME/.config"
+
+# Clone repo
 git clone --bare --branch master --depth 1 https://github.com/i13e/cfg.git "$HOME/.config/cfg"
 #--recursive?
 
@@ -15,12 +45,12 @@ cfg() { /usr/bin/git --git-dir="$HOME/.config/cfg/" --work-tree="$HOME" "$@"; }
 mkdir -p "$HOME/.cfg-backup/"
 
 if cfg checkout; then
-  echo "Successfully checked out dotfiles.";
+  echo "Successfully checked out dotfiles."
   else
-    echo "Backing up pre-existing dotfiles.";
+    echo "Backing up pre-existing dotfiles."
     cfg checkout 2>&1 | grep -E "\s+\." | awk "{print $1}" | xargs -I{} mv {} "$HOME/.cfg-backup/"
-    cfg checkout && echo "Successfully checked out dotfiles.";
-fi;
+    cfg checkout && echo "Successfully checked out dotfiles."
+fi
 
 cfg config status.showUntrackedFiles no
 
@@ -36,32 +66,13 @@ cfg config status.showUntrackedFiles no
 # update submodules
 cfg submodule update --init
 
+## Install Repo's Package List.
 
-# check that you are on archlinux?
-# WARNING: Do not run the rest of this script unless you know EXACTLY what
-#you are doing. It is only recommended for a clean install of Arch Linux.
-#Do you wish to continue?
-# if yes continue
-# if no, exit
+    # Install packages
+    # sudo pacman -S --needed --noconfirm $(cat pkglist.txt)
+    # Install packages from AUR
+    # git clone https://aur.archlinux.org/paru-bin.git "/tmp/paru-bin"
 
-# install paru
-git clone https://aur.archlinux.org/paru-bin.git
-cd paru-bin || exit # exit in case cd fails
-makepkg -si --noconfirm
-cd "$HOME" && rm -rf paru-bin
-
-# confirm script from hostupd
-#while true; do
-#    printf '%b ' "\033[1m":: Do you wish to use a custom hosts file? [Y/n]"\033[0m"
-#    read -r yn
-#    case $yn in
-#        [Yy]* ) sudo tee -a /etc/hosts < "$XDG_DATA_HOME/hosts" > /dev/null; exit;;
-#        [Nn]* ) exit;;
-#        * ) echo "Please answer y or n.";;
-#    esac
-#done
-
-# install from repo's packagelist
 # paru -S fonts, neovim, symlinks, compositor, scripts, tools, browser, zsh, etc
 # would you like to use your own package list or install the repo default?
 # install pkg list from repo
@@ -69,8 +80,10 @@ cd "$HOME" && rm -rf paru-bin
 
 #printf "Password: " ; read -r myPass ; echo "Password is: $myPass"
 
-# if zsh was installed change the shell to zsh
-if command -v curl >/dev/null 2>&1; then
+## Wrap up.
+
+# Change the $USER's shell to Zsh
+if command -v zsh >/dev/null 2>&1; then
     echo "changing default shell to zsh…"
     sudo usermod -s "$(which zsh)" "$USER"
 else
