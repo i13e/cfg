3e2fd1320e7846886e9c37fe9b6b9effd5e8cc02
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2022-08-10 10:23:54 -0400

    add autocmd.lua

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,143 +1,229 @@
--- Last Change: Fri, 29 Apr 2022 14:34
+-- source: https://bit.ly/3x8oiNR
 
---- This function is taken from https://github.com/norcalli/nvim_utils
-function nvim_create_augroups(definitions)
-    for group_name, definition in pairs(definitions) do
-        vim.api.nvim_command("augroup " .. group_name)
-        vim.api.nvim_command("autocmd!")
-        for _, def in ipairs(definition) do
-            local command = table.concat(vim.tbl_flatten({ "autocmd", def }), " ")
-            vim.api.nvim_command(command)
-        end
-        vim.api.nvim_command("augroup END")
-    end
-end
+local augroups = {}
 
-local autocmds = {
-    reload_vimrc = {
-        -- Reload vim config automatically
-        -- {"BufWritePost",[[$VIM_PATH/{*.vim,*.yaml,vimrc} nested source $MYVIMRC | redraw]]};
-        --{ "BufWritePre", "$MYVIMRC", "lua require('core.utils').ReloadConfig()" },
-    },
-    general_settings = {
-        { "Filetype", "qf,help,man,lspinfo", ":nnoremap <silent> <buffer> q :close<CR>" },
-        { "Filetype", "qf", ":set nobuflisted" },
-    },
-    -- https://www.reddit.com/r/vim/comments/4aab93/comment/d0yo622/
-    autoquickfix = {
-        { "QuickFixCmdPost", "]^l]*", "cwindow" },
-        { "QuickFixCmdPost", "l*", "lwindow" },
-    },
-    -- wrap_spell = {
-    --     { "FileType", "markdown", ":setlocal wrap" },
-    --     { "FileType", "markdown", ":setlocal spell" },
-    -- },
-    fix_commentstring = {
-        { "Bufenter", "*config,*rc,*conf", "set commentstring=#%s" },
-    },
-    reload_sxhkd_bindings = {
-        {'BufWritePost', '*sxhkdrc', '!pkill -USR1 sxhkd'},
-        -- {'BufWritePost', '*bspwmrc', '!bspc wm -r; dunstify "bspwm restarted!"'},
-    },
-    make_scripts_executable = {
-        {'BufWritePost', '*.sh', [[!chmod +x %]]},
-        {'BufWritePost', '*.py', [[!chmod +x %]]},
-    },
-    custom_updates = {
-        {"BufRead,BufNewFile", "Xresources,Xdefaults,xresources,xdefaults", "set filetype=xdefaults"},
-        {"BufWritePost", "Xresources,Xdefaults,xresources,xdefaults", "!xrdb %"},
-        {'BufWritePost', 'fonts.conf', '!fc-cache'},
-        {'BufWritePost', 'config.py', '!python -m py_compile ~/.config/qtile/config.py'},
-    },
-    -- https://stackoverflow.com/a/23326474/2571881
-    format_options = { -- :h fo-talbe (for help)
-        { "BufWinEnter,BufRead,BufNewFile", "*", "setlocal formatoptions-=r formatoptions-=o" },
-    },
-    change_header = {
-         { "BufWritePre", "*", "lua require('core.utils').changeheader()" },
-    },
-    resize_windows_proportionally = {
-        { "VimResized", "*", ":wincmd =" };
-	{ "Filetype", "help", ":wincmd =" };
-    };
-    packer = {
-        { "BufWritePost", "plugins.lua", "source <afile> | PackerCompile" },
-    },
-    terminal_job = {
-        { "TermOpen", "*", [[tnoremap <buffer> <Esc> <c-\><c-n>]] },
-        { "TermOpen", "*", [[tnoremap <buffer> <leader>x <c-\><c-n>:bd!<cr>]] },
-        { "TermOpen", "*", [[tnoremap <expr> <A-r> '<c-\><c-n>"'.nr2char(getchar()).'pi' ]] },
-        { "TermOpen", "*", "startinsert" },
-        { "TermOpen", "*", [[nnoremap <buffer> <C-c> i<C-c>]] },
-        { "TermOpen", "*", "setlocal listchars= nonumber norelativenumber" },
-    },
-    restore_cursor = {
-        { "BufRead", "*", [[call setpos(".", getpos("'\""))]] },
-    },
-
-    -- https://vi.stackexchange.com/a/25687/7339
-    -- hicurrent_word = {
-    --     { "CursorHold", "*", [[:exec 'match Search /\V\<' . expand('<cword>') . '\>/']] },
-    -- },
-    -- save_shada = {
-    --     -- { "VimLeave", "*", "wshada!"};
-    --     { "CursorHold", "*", [[rshada|wshada]]};
-    -- };
-    -- auto_exit_insertmode = {
-    --     { "CursorHoldI", "*", "stopinsert" },
-    -- },
-    wins = {
-        { "VimResized", "*", ":wincmd =" },
-        { "WinEnter", "*", "wincmd =" },
-        { "BufEnter", "NvimTree", [[setlocal cursorline]] },
-    },
-    toggle_search_highlighting = {
-        { "InsertEnter", "*", "setlocal nohlsearch" },
-    },
-    lua_highlight = {
-        { "TextYankPost", "*", [[silent! lua vim.highlight.on_yank() {higroup="IncSearch", timeout=600}]] },
-    },
-    auto_working_directory = {
-        { "BufEnter", "*", "silent! lcd %:p:h" },
-    },
-    clean_trailing_spaces = {
-        --{ "BufWritePre", "*", [[lua require("core.utils").preserve('%s/\\s\\+$//ge')]] },
-        { "BufWritePre", "*", ":%s/\\s\\+$//e" },
-        { "BufWritePre", "*", "%s/\\n\\+\\%$//e" },
-        { "BufWritePre", "*.[ch]", "%s/\\%$/\r/e"},
-
-    },
-    -- ansi_esc_log = {
-    --     { "BufEnter", "*.log", ":AnsiEsc" };
-    -- };
-
-    -- https://stackoverflow.com/a/18427760/2571881
-    -- https://vimhelp.org/usr_11.txt.html
-    -- https://ttm.github.io/research/2017/11/02/vim-swp-swo.html
-    -- AutoRecoverSwapFile = {
-    --     { "SwapExists", "*", [[let v:swapchoice = 'r' | let b:swapname = v:swapname]] },
-    --     { "BufWinEnter", "*", [[if exists("b:swapname") | call delete(b:swapname) | endif]] },
-    -- },
-    flash_cursor_line = {
-        { "WinEnter", "*", "lua require('core.utils').flash_cursorline()" },
-        -- { "WinEnter", "*", "Beacon" },
-        -- { "VimEnter", "diary.md", "0put=strftime('%c')" },
-    },
-    -- attatch_colorizer = {
-    -- 	{BufReadPost *.conf setl ft=conf};
-    -- 	{"BufReadPost", "config", "setl ft=conf"};
-    -- 	{"FileType", "conf", "ColorizerAttachToBuffer<CR>"};
-    -- };
+augroups.buf_write_pre = {
+	mkdir_before_saving = {
+		event = { "BufWritePre", "FileWritePre" },
+		pattern = "*",
+		-- TODO: Replace vimscript function
+		command = [[ silent! call mkdir(expand("<afile>:p:h"), "p") ]],
+	},
+	clear_search_highlighting = {
+		event = "InsertEnter",
+		pattern = "*",
+		callback = function()
+			vim.opt_local.hlsearch = false
+			vim.fn.clearmatches()
+		end,
+	},
 }
 
--- vim.cmd [[
--- "Auto-deletes all trailing whitespace and newlines at EOF on save & resets cursor position
---   autocmd BufWritePre * let currPos = getpos(".")
---   autocmd BufWritePre * %s/\s\+$//e
---   autocmd BufWritePre * %s/\n\+\%$//e
---   autocmd BufWritePre *.[ch] %s/\%$/\r/e
---   autocmd BufWritePre * cal cursor(currPos[1], currPos[2])
--- ]]
+augroups.misc = {
 
-nvim_create_augroups(autocmds)
--- autocommands END
+	-- https://stackoverflow.com/a/60338380/2571881
+	-- Mode_changed = {
+	-- 	event = "ModeChanged",
+	-- 	pattern = { "*:i*", "i*:*" },
+	-- 	callback = function()
+	-- 		if vim.bo.filetype ~= "markdown" then
+	-- 			vim.o.relativenumber = vim.v.event.new_mode:match("^i") == nil
+	-- 		end
+	-- 	end,
+	-- },
+
+	-- change_header = {
+	-- 	event = "BufWritePre",
+	-- 	pattern = "*",
+	-- 	callback = function()
+	-- 		require("utils").changeheader()
+	-- 	end,
+	-- },
+
+	clean_trailing_spaces = {
+		event = "BufWritePre",
+		pattern = "*",
+		callback = function()
+			vim.cmd([[%s/\s\+$//e]])
+			vim.cmd([[%s/\n\+\%$//e]])
+			--require("utils").preserve([[%s/\s\+$//e]])
+			--require("utils").preserve([[%s/\n\+\%$//e]])
+			-- { "BufWritePre", "*.[ch]", [[%s/\%$/\r/e]] },
+		end,
+	},
+
+	-- TODO : delete these 3 autocmds
+	-- update_file = { -- If you stop typing for a while it will save it!
+	--     event = {"CursorHold", "CursorHoldI"},
+	--     pattern = "*",
+	--     callback = function()
+	--         -- https://stackoverflow.com/a/69669289/2571881
+	--         local buffer_name = vim.api.nvim_buf_get_name(0)
+	--         if (buffer_name ~= '' or ~= nil) then
+	--             vim.cmd('<buffer> silent! update')
+	--         end
+	--     end
+	-- },
+
+	-- -- https://www.reddit.com/r/neovim/comments/mn7coe/
+	-- lsp_diagnostic = {
+	--     event = "CursorHold",
+	--     pattern = "*",
+	--     callback = function()
+	--         vim.lsp.diagnostic.show_line_diagnostics()
+	--     end,
+	-- },
+	--
+	-- ls_signature = {
+	--     event = "CursorHoldI",
+	--     pattern = "*",
+	--     callback = function()
+	--         vim.lsp.buf.signature_help()
+	--     end,
+	-- },
+
+	unlist_terminal = {
+		event = "TermOpen",
+		pattern = "*",
+		command = [[
+    tnoremap <buffer> <Esc> <c-\><c-n>
+    tnoremap <buffer> <leader>x <c-\><c-n>:bd!<cr>
+    tnoremap <expr> <A-r> '<c-\><c-n>"'.nr2char(getchar()).'pi'
+    startinsert
+    nnoremap <buffer> <C-c> i<C-c>
+    setlocal listchars= nonumber norelativenumber
+    setlocal nobuflisted
+    ]],
+	},
+
+	fix_commentstring = {
+		event = "BufEnter",
+		pattern = "*config,*rc,*conf,sxhkdrc,bspwmrc",
+		callback = function()
+			vim.bo.commentstring = "#%s"
+			vim.cmd("set syntax=config")
+		end,
+	},
+
+	-- reload_myvimrc = {
+	--     event = "BufWritePre",
+	--     pattern = "$MYVIMRC",
+	--     callback = function()
+	--     ReloadConfig()
+	--     end,
+	-- },
+
+	compile_python = {
+		event = "BufWritePost",
+		pattern = "*.py",
+		command = [[!python -m py_compile %]],
+	},
+
+	reload_sxhkd = {
+		event = "BufWritePost",
+		pattern = "sxhkdrc",
+		command = [[!pkill -USR1 -x sxhkd]],
+	},
+
+	make_scripts_executable = {
+		event = "BufWritePost",
+		pattern = "*.{sh,py,zsh}",
+		callback = function()
+			local file = vim.fn.expand("%p")
+			local status = require("core.utils").is_executable()
+			if status ~= true then
+				vim.fn.setfperm(file, "rwxr-x---")
+			end
+		end,
+	},
+
+	update_xrdb = {
+		event = "BufWritePost",
+		pattern = "Xresources,Xdefaults,xresources,xdefaults",
+		command = [[!xrdb -merge %]],
+	},
+	updated_xdefaults = {
+		event = "BufWritePost",
+		pattern = "~/.Xdefaults",
+		command = [[!xrdb -merge ~/.Xdefaults]],
+	},
+
+	restore_cursor_position = {
+		event = "BufRead",
+		pattern = "*",
+		command = [[call setpos(".", getpos("'\""))]],
+	},
+
+	lwindow_quickfix = {
+		event = "QuickFixCmdPost",
+		pattern = "l*",
+		command = [[lwindow | wincmd j]],
+	},
+
+	cwindow_quickfix = {
+		event = "QuickFixCmdPost",
+		pattern = "[^l]*",
+		command = [[cwindow | wincmd j]],
+	},
+	-- auto_working_directory = {
+	--     event = "BufEnter",
+	--     pattern = "*",
+	--     callback = function()
+	--         vim.cmd("silent! lcd %:p:h")
+	--     end,
+	-- },
+}
+
+augroups.yankpost = {
+
+	save_cursor_position = {
+		event = { "VimEnter", "CursorMoved" },
+		pattern = "*",
+		callback = function()
+			cursor_pos = vim.fn.getpos(".")
+		end,
+	},
+
+	highlight_yank = {
+		event = "TextYankPost",
+		pattern = "*",
+		callback = function()
+			vim.highlight.on_yank({ higroup = "IncSearch", timeout = 400, on_visual = true })
+		end,
+	},
+
+	yank_restore_cursor = {
+		event = "TextYankPost",
+		pattern = "*",
+		callback = function()
+			local cursor = vim.fn.getpos(".")
+			if vim.v.event.operator == "y" then
+				vim.fn.setpos(".", cursor_pos)
+			end
+		end,
+	},
+}
+
+-- Note: some Filetype's have their own ftplugin, that's why you cannot see help and qf here
+augroups.quit = {
+	quit_with_q = {
+		event = "FileType",
+		pattern = { "checkhealth", "fugitive", "git*", "lspinfo" },
+		callback = function()
+			-- vim.api.nvim_win_close(0, true) -- TODO: Replace vim command with this
+			vim.api.nvim_buf_set_keymap(0, "n", "q", "<cmd>close!<cr>", { noremap = true, silent = true })
+			vim.bo.buflisted = false
+		end,
+	},
+}
+
+for group, commands in pairs(augroups) do
+	local augroup = vim.api.nvim_create_augroup("AU_" .. group, { clear = true })
+
+	for _, opts in pairs(commands) do
+		local event = opts.event
+		opts.event = nil
+		opts.group = augroup
+		vim.api.nvim_create_autocmd(event, opts)
+	end
+end
