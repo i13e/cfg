8cba9ec1d51e627cd43b85a4de7babb5cbc2db59
Ian Breckenridge <62034540+i13e@users.noreply.github.com>
2023-10-12 09:40:56 -0400

    feat: Add wezterm config and update LazyVim

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,27 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "nord"
+wezterm.font = wezterm.font_with_fallback({
+	{ family = "Monospace", scale = 1.0 },
+})
+config.window_background_opacity = 0.85
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "AlwaysPrompt"
+config.scrollback_lines = 10000
+config.default_workspace = "home"
+
+-- and finally, return the configuration to wezterm
+return config
